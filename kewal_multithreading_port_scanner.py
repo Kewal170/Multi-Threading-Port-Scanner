1dcbfdb56ae2a9b01635f12195afe8ac866992cb
Kewal Suthar <102758565+Kewal170@users.noreply.github.com>
2022-08-10 13:21:14 +0530

    Update kewal_multithreading_port_scanner.py

--- kewal_multithreading_port_scanner.py
+++ kewal_multithreading_port_scanner.py
@@ -1,37 +1,40 @@
+import queue
 import socket
 import threading
-import queue
 
-ip_address = '<ip address>'
+ip_address = '192.168.56.1'
 q = queue.Queue()
 
 
-def banner(ip_address): # Banner
-	print("-"*100)
-	print("Scanning {}".format(ip_address))
-	print("-"*100)
+def banner(ip):  # Banner
+    print("-" * 100)
+    print(f"[*] Scanning {ip}")
+    print("-" * 100)
 
-for i in range(1,501): # Storing 500 ports in our queue
-	q.put(i)
 
-def scan(): # Defining scan option
-	while not q.empty():
-		protocolname = 'tcp'
-		port = q.get()
-		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-			try:
-				s.connect((ip_address,port))
-				SERVICE = socket.getservbyport(port, protocolname)
-				# print("Port: %s => service name: %s" %(port, SERVICE) 
-				print(f"[+] Port {port} :: {SERVICE}")
-			except:
-				pass
-		q.task_done()
+for i in range(1, 1001):  # Storing 1000 ports in our queue
+    q.put(i)
 
-for i in range(50): # Create number of threads we want to see
-	t = threading.Thread(target=scan, daemon=True)
-	t.start()
+
+def scan():  # Defining scan option
+    while not q.empty():
+        protocol_name = 'tcp'
+        port = q.get()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.connect((ip_address, port))
+                service = socket.getservbyport(port, protocol_name)
+                print(f"[+] Port {port} :: {service}")
+            except:
+                pass
+        q.task_done()
+
+
+for i in range(100):  # Create number of threads we want to see
+    t = threading.Thread(target=scan, daemon=True)
+    t.start()
 
 banner(ip_address)
 q.join()
+print("-"*50)
 print("Finished scanning")
